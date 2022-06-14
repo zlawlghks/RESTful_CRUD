956e299506f62bf6bfe3593e8dec75674a00b59a
zlawlghks <rlawlghks69@naver.com>
2022-06-15 00:24:34 +0900

    Feat: 필드 내용 일부 삭제

--- restful-crud/src/main/java/zlawlghks/restfulcrud/Domain/Post.java
+++ restful-crud/src/main/java/zlawlghks/restfulcrud/Domain/Post.java
@@ -19,8 +19,7 @@ public class Post {
     @Id
     @GeneratedValue
     private Integer id; // 기본 값
-    private String userName; // 작성자 이름
-    private String boardName; // 게시물 이름
     private String description; // 게시물 내용
+    private String boardName; // 게시물 이름
     private Date joinDate; // 등록 시간
 }
