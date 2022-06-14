0776ddb1643a193d2eb93ae1e1d3a5b2d5263fbe
zlawlghks <rlawlghks69@naver.com>
2022-06-15 01:27:46 +0900

    Feat: 게시글 상세 조회

--- restful-crud/src/main/java/zlawlghks/restfulcrud/Controller/PostController.java
+++ restful-crud/src/main/java/zlawlghks/restfulcrud/Controller/PostController.java
@@ -1,11 +1,20 @@
 package zlawlghks.restfulcrud.Controller;
 
+import org.hibernate.EntityMode;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import zlawlghks.restfulcrud.Domain.Post;
+import zlawlghks.restfulcrud.PostNotFoundException;
 import zlawlghks.restfulcrud.Repository.PostRepository;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class PostController {
@@ -13,8 +22,27 @@ public class PostController {
     private PostRepository postRepository;
 
     // 게시글 목록 조회
-    @GetMapping
+    @GetMapping("/posts")
     public List<Post> retrieveAllPost() {
         return postRepository.findAll();
     }
+
+    // 게시글 상세 조회
+    @GetMapping("/posts/{id}")
+    public EntityModel<Post> retrievePost(@PathVariable int id) {
+        Optional<Post> post = postRepository.findById(id);
+
+        if (!post.isPresent()) {
+            throw new PostNotFoundException(String.format("ID{%s} not found", id));
+        }
+
+        EntityModel<Post> entityModel = EntityModel.of(post.get());
+        WebMvcLinkBuilder linkto = linkTo(methodOn(this.getClass()).retrieveAllPost());
+        entityModel.add(linkto.withRel("all-posts"));
+
+        return entityModel;
+    }
+
+
+
 }
