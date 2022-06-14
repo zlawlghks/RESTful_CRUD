e82364a9637f8591e277e825731e7f7e6399c9c6
zlawlghks <rlawlghks69@naver.com>
2022-06-14 22:53:55 +0900

    2022.06.14

---
+++ restful-crud/src/main/java/zlawlghks/restfulcrud/RestfulCrudApplication.java
@@ -0,0 +1,13 @@
+package zlawlghks.restfulcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulCrudApplication.class, args);
+	}
+
+}
---
+++ restful-crud/src/test/java/zlawlghks/restfulcrud/RestfulCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package zlawlghks.restfulcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
